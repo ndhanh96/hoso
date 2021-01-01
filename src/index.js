6cf7e9909a2095aea48f3e901f244740d9566501
ndhanh96 <ndhanh96@gmail.com>
2021-01-02 04:49:01 +0700

    barebone update.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-
-class ContentBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
-  render() { 
-    return (  
-      <div></div>
-    );
-  }
-}
+let backgroundpagecolor = 'rgba(245, 194, 87, 1)';
 
 class HandleSwitchPage extends React.Component {
   constructor(props) {
@@ -24,20 +13,123 @@ class HandleSwitchPage extends React.Component {
 
   handleClick(e) {
     // alert(this.props.page);
-    switch (this.props.page) {
-      case 'home':
-        e.target.value = 'Home';
+    switch (e.target.value) {
+      case 'Home':
+        e.target.value = 'home';
+        backgroundpagecolor = 'rgba(245, 194, 87, 1)';
         break;
+      case 'About':
+        e.target.value = 'about';
+        backgroundpagecolor = 'red';
+        break;
+      case 'Project':
+        e.target.value = 'project';
+        backgroundpagecolor = 'grey';
+        break;
+      case 'Contact':
+        e.target.value = 'contact';
+        backgroundpagecolor = 'cyan';
+        break;
+      default :
+        e.target.value = 'testing';
     }
     this.props.info(e.target.value);
   }
 
   render() {
+    let colorhome = '';  let colorabout = ''; let colorproject = ''; let colorcontact = '';
+    switch (this.props.page) { 
+      case 'home':
+        colorhome = '#F5C257';
+        break;
+      case 'about':
+        colorabout = '#F5C257';
+        break;
+      case 'contact':
+        colorcontact = '#F5C257';
+        break;
+      case 'project':
+        colorproject = '#F5C257';
+        break;
+        
+    }
+     
     return (
-      // <li><input type="button" value={this.props.name} onClick={(e) => this.handleClick(e)} /></li>
-      <li><input type="button" value={this.props.page} onClick={this.handleClick} /></li>
+      <ul className="menuNavBar">
+        <li><input style={{backgroundColor:colorhome}} type="button" value={'Home'} onClick={this.handleClick} /></li>
+        <li><input style={{backgroundColor:colorabout}} type="button" value={'About'} onClick={this.handleClick} /></li>
+        <li><input style={{backgroundColor:colorproject}} type="button" value={'Project'} onClick={this.handleClick} /></li>
+        <li><input style={{backgroundColor:colorcontact}} type="button" value={'Contact'} onClick={this.handleClick} /></li>
+      </ul>
     );
   }
+}
+
+function ContentBox(props) {
+
+  let value = '';
+  let imgPage = 'car.png';
+
+  function GenerateContent() {
+    switch(props.page) {
+      case 'home':
+        value = 'welcome to my newly created portfolio';
+        imgPage = 'car.png';
+        break;
+      case 'about':
+        value = 'your friendly candidate . still waiting my shot';
+        imgPage = 'car.png';
+        break;
+      case 'project':
+        value = 'behold my latest stupid creation';
+        imgPage = 'car.png';
+        break;
+      case 'contact':
+        value = 'hit me by email so we can talk properly';
+        imgPage = 'contact.png';
+        break;
+    }
+    return (
+      <h1>{value}</h1>
+    );
+  }
+
+  function ChangeImageContent() {
+    switch(props.page) {
+      case 'home':
+        imgPage = 'home.png';
+        break;
+      case 'about':
+        imgPage = 'aboutme.png';
+        break;
+      case 'project':
+        imgPage = 'car.png';
+        break;
+      case 'contact':
+        imgPage = 'contact.png';
+        break;
+    }
+    return (
+      <img src={imgPage} alt=''/>
+    );
+  }
+
+  return (  
+    <div className="contentBoxOuter">
+      <div className="contentBox">
+        <GenerateContent />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation.</p>
+      </div>
+      <div style={{backgroundColor: backgroundpagecolor}} className="imgAvatarBox">
+        <ChangeImageContent />
+      </div>
+    </div>
+  );
 }
  
 class HomePage extends React.Component {
@@ -50,7 +142,6 @@ class HomePage extends React.Component {
   }
 
   handleChange(value){
-    alert(value);
     this.setState({
       page: value,
     });
@@ -61,16 +152,9 @@ class HomePage extends React.Component {
       <div className="innerBox">
         <div id="navBar">
           <img id="logo" src="logo.png" alt="portfolio" />
-          <ul className="menuNavBar">
-            <HandleSwitchPage page={this.state.page} info={this.handleChange} />
-            <li><a href="">About</a></li>
-            <li><a href="">Project</a></li>
-            <li><a href="">Contact</a></li>
-          </ul>
+          <HandleSwitchPage page={this.state.page} info={this.handleChange} />
         </div>
-        <div className="contentBox">
-          <ContentBox switchpage={this.state.page} />
-        </div>
+        <ContentBox page={this.state.page} />
       </div>
     );
   }
